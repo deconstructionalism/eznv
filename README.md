51d33d069809a27f9f422b0f5825d52b1e6736f3
deconstructionalism <deconstructionalism@gmail.com>
2018-05-16 18:18:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# EZNV 
+
+EZ backup and restore of your computing NVironment
+
+## Purpose
+
+Backup important config files and program-installed dependency lists to a
+private gist automatically, and restore installation easily as well.
+
+## Requirements
+
+- MacOS 10 (only tested on 10.13.4)
+- Python 3.6
+- `gist` command line gister (https://github.com/defunkt/gist)
+- binaries listed in **GENERATE INSTALL LIST FILES** section of `eznv_backup.sh`
+- config files listed in **CONFIG FILES TO SAVE** section of `eznv_backup.sh`
+
+## Setup
+
+1. Download or Fork and Clone the contents of this repo
+2. Edit the following in `eznv_backup.sh` to suit your needs:
+   - **CONFIG FILES TO SAVE** section: include configuration files you want backed up
+     > these will not be re-installed on restore, just backed up
+   - **GENERATE INSTALL LIST FILES** section: edit expressions to generate install lists for programs you want backed up
+     - expressions must output a file with a `[program_name].install` formatted name
+     - items in `[program_name].install` must be on their own line
+3. Edit `restore_installers.json` to suit your needs:
+    - You can add installer configurations for restoring from `*.install` files saved by `eznv_backup.sh`. The `restore_installers.json` file in root dir is set up in the following format:
+    ```json
+    {
+        [program_name]: {
+            "sh_item_command": [shell_command]
+        },
+        ...
+    }
+    ```
+    - program_name =  `*` in `*.install`
+    - shell_command = shell comand that would be run on each item in `*.install` list with `{item}` added to command where item from list would normally appear within command
+    - **EXAMPLE**:
+    ```json
+    {
+        "brew": {
+            "sh_item_command": "brew install {item}"
+        },
+        ...
+    }
+    ```
+## Useage
+
+### Backup
+- run `sh eznv_backup.sh`
+- once backup is complete, your backup gist will be opened in a new browser window
+
+### Restore
+- run `python3 eznv_restore.sh [gist_id]`
---
+++ eznv_backup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# ----------- #
+# EZNV BACKUP #
+# ----------- #
+
+# AUTHOR: Arjun Ray
+# EMAIL: deconstructionalism@gmail.com
+# LICENSE: GNU GPLv3
+# MAINTAINER: Arjun Ray
+# STATUS: Development
+# VERSION: 0.0.1
+
+# PURPOSE: Backup important config files and program 
+# installed dependency lists to a private gist 
+# automatically.  Allows for easy restore of environment
+# using the `eznv_restore.py` script.
+
+# REQUIREMENTS:
+# - MacOS 10 (only tested on 10.13.4)
+# - binaries listed in "GENERATE INSTALL LIST FILES" section
+# - config files listed in "CONFIG FILES TO SAVE" section
+# - gist (https://github.com/defunkt/gist)
+
+# IMPORTANT: Do not rename `config_files`
+
+# YOU CAN:
+#  - add files you want to backup to `config_files`
+#  - add expressions to generate install lists for
+#    programs in "GENERATE INSTALL LIST FILES" section
+#    - expressions must output a file with a 
+#      "[program_name].install" formatted name
+#  - add a configuration for a given "*.install" file generated 
+#    by this script to `restore_installers.json` to be used
+#    by `eznv_restore.py` (see doc for that script 
+#    for details)
+
+
+# GENERATE SYSTEM INFO FILE
+cat << EOF > ~/._system.backup
+SYSTEM BACKUP
+-------------
+BACKUP DATE: $(date '+%d-%m-%Y %H:%M:%S')
+$(uname -a)
+$(sw_vers)
+EOF
+
+# CONFIG FILES TO SAVE
+config_files=("~/._system.backup" \
+              "~/com.googlecode.iterm2.plist" \
+              "~/.zshrc" \
+              "~/.bashrc" \
+              "~/.bash_profile" \
+              "~/.vimrc" \
+              "~/.bashrc" \
+              "~/.config/nvim/init.vim")
+
+# GET PATH OF THIS SCRIPT
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+self_path="$dir/`basename $0`"
+
+# MAKE TEMP DIR FOR INSTALL LIST FILES
+tmp_dir="/tmp/`date \"+CONFIG_BACKUP.%y-%m-%d.%H-%M-%S\"`"
+mkdir $tmp_dir
+cd $tmp_dir
+
+# GENERATE INSTALL LIST FILES
+apm list --installed --bare > apm.install
+code --list-extensions > code.install
+brew leaves > brew.install
+brew cask list --full-name > brew_cask.install
+system_profiler SPApplicationsDataType -xml > apps.install
+
+# GET SPACE-SEPARATED LIST OF INSTALL LIST FILES
+install_files=$(find  *.install | tr '\n' ' ')
+
+# MAKE PRIVATE GIST WITH ALL FILES
+# includes: config_files, install list files, this script
+gist -p -o $self_path $install_files ${config_files[@]} -d "`date` - full config backup"
